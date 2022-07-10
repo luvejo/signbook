7167155c21f71b8627cb00d78f5b288da339e892
Luis Velásquez <luvejo.dev@gmail.com>
2022-07-10 18:04:30 -0500

    feat: add /home and /new-sign pages

---
+++ components/common/BaseLoading.jsx
@@ -0,0 +1,12 @@
+function BaseLoading() {
+  return (
+    <div
+      className="min-h-screen w-screen flex justify-center
+  items-center"
+    >
+      Loading signs from hasura...
+    </div>
+  )
+}
+
+export default BaseLoading
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '@/lib/apollo'
+import Head from 'next/head'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <title>Signbook</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://unpkg.com/tailwindcss@^3/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
